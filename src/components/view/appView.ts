eb91eac1cbd092d512689fd7e9aafaaefc360a8d
EkaterinaMash <ekaterina.mashinskayaa@gmail.com>
2023-06-09 16:16:20 +0300

    feat: changes sources container style

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -12,13 +12,11 @@ export class AppView {
     }
 
     drawNews(data: Data) {
-        console.log(1, data);
         const values: Array<NewsObject> = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
     drawSources(data: Data) {
-        console.log(2, data);
         const values: Array<SourcesObject> = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
